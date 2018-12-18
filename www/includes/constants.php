0f0682975f8fe1cb69cecbfcf48128a0fe5bc4d2
Raül Ojeda <raulojeda10g@gmail.com>
2018-12-18 18:39:06 +0100

    Change to ssl

--- www/includes/constants.php
+++ www/includes/constants.php
@@ -5,8 +5,8 @@ if ($_SERVER['HTTP_HOST']=='localhost'||$_SERVER['HTTP_HOST']=='127.0.0.1'){
     define('_PUBLIC_URL_','http://'.$_SERVER['HTTP_HOST'].'/hellscripts/www');
     define('_PROJECT_URL_','http://'.$_SERVER['HTTP_HOST'].'/hellscripts/www');    
 }else {
-    define('_PUBLIC_URL_','http://'.$_SERVER['HTTP_HOST']);
-    define('_PROJECT_URL_','http://'.$_SERVER['HTTP_HOST'].'/www');    
+    define('_PUBLIC_URL_','https://'.$_SERVER['HTTP_HOST']);
+    define('_PROJECT_URL_','https://'.$_SERVER['HTTP_HOST'].'/www');    
 }
 
 
