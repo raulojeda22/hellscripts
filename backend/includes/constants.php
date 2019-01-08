e1d8a5c96adc1e33a8780dcdc6a0bc74c4599186
Raül Ojeda <raulojeda10g@gmail.com>
2019-01-08 20:52:49 +0100

    Error url en local

--- backend/includes/constants.php
+++ backend/includes/constants.php
@@ -7,10 +7,10 @@ if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
 }
 
 if ($_SERVER['HTTP_HOST']=='localhost'||$_SERVER['HTTP_HOST']=='127.0.0.1'){
-    define('_PUBLIC_URL_',$protocol.$_SERVER['HTTP_HOST'].'/hellscripts/www');
+    define('_PUBLIC_URL_',$protocol.$_SERVER['HTTP_HOST'].'/hellscripts');
     define('_PROJECT_URL_',$protocol.$_SERVER['HTTP_HOST'].'/hellscripts/www');    
 }else {
-    define('_PUBLIC_URL_',$protocol.$_SERVER['HTTP_HOST'].'/www');
+    define('_PUBLIC_URL_',$protocol.$_SERVER['HTTP_HOST']);
     define('_PROJECT_URL_',$protocol.$_SERVER['HTTP_HOST'].'/www');    
 }
 
