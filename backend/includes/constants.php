d5418c4b65b7b785128513325972085459c4f664
Raül Ojeda <raulojeda10g@gmail.com>
2019-01-08 15:51:00 +0100

    La carpeta principal ha de anomenar-se www

--- backend/includes/constants.php
+++ backend/includes/constants.php
@@ -7,11 +7,11 @@ if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
 }
 
 if ($_SERVER['HTTP_HOST']=='localhost'||$_SERVER['HTTP_HOST']=='127.0.0.1'){
-    define('_PUBLIC_URL_',$protocol.$_SERVER['HTTP_HOST'].'/hellscripts/frontend');
-    define('_PROJECT_URL_',$protocol.$_SERVER['HTTP_HOST'].'/hellscripts/frontend');    
+    define('_PUBLIC_URL_',$protocol.$_SERVER['HTTP_HOST'].'/hellscripts/www');
+    define('_PROJECT_URL_',$protocol.$_SERVER['HTTP_HOST'].'/hellscripts/www');    
 }else {
     define('_PUBLIC_URL_',$protocol.$_SERVER['HTTP_HOST']);
-    define('_PROJECT_URL_',$protocol.$_SERVER['HTTP_HOST'].'/frontend');    
+    define('_PROJECT_URL_',$protocol.$_SERVER['HTTP_HOST'].'/www');    
 }
 
 
