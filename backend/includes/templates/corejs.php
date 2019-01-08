0b36bab79d043b613a102745b6b306c0417803e4
Raül Ojeda <raulojeda10g@gmail.com>
2019-01-08 20:55:13 +0100

    Error de public url en local

--- backend/includes/templates/corejs.php
+++ backend/includes/templates/corejs.php
@@ -1,3 +1,3 @@
-<script src="<? echo _PUBLIC_URL_ ?>/js/scripts.min.js"></script>
-<script src="<? echo _PUBLIC_URL_ ?>/js/main.min.js"></script>
-<script src="<? echo _PUBLIC_URL_ ?>/js/custom.js"></script>
+<script src="<? echo _PROJECT_URL_ ?>/js/scripts.min.js"></script>
+<script src="<? echo _PROJECT_URL_ ?>/js/main.min.js"></script>
+<script src="<? echo _PROJECT_URL_ ?>/js/custom.js"></script>
