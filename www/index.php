89cd2b819e89b82db82ac88c77c7e21eadf8977c
Raül Ojeda <raulojeda10g@gmail.com>
2019-01-08 16:06:20 +0100

    Prova claus github

--- www/index.php
+++ www/index.php
@@ -2,5 +2,5 @@
 include_once '../backend/includes/constants.php';
 include_once '../backend/controllers/FrontController.class.php';
 $frontController=new FrontController;
-$frontController->run(); #PROVA
+$frontController->run();
 ?>
