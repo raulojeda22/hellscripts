b1bcd3896d14ff609b5457f8f96122f6a4743bb9
Raül Ojeda <raulojeda10g@gmail.com>
2019-01-10 09:00:04 +0100

    Model Controller més avançat

--- backend/models/ControllerCore.class.php
+++ backend/models/ControllerCore.class.php
@@ -1,21 +1,67 @@
 <?
 class ControllerCore{
+
+    private function addWhereStatement($array){
+        $query = "";
+        foreach ($array as $row => $value){
+            $query .= ' WHERE '.$row.'='.$value;
+            if ($conditions>0){
+                $query .= ' AND';
+            }
+            $conditions++;
+        }
+        return $query;
+    }
+
     protected function runQuery($query){
         $connection = Connection::connect();
         $response = mysqli_query($connection, $query);
         Connection::close($connection);
         return $response;
     }
+
     protected function buildGetQuery($data){
-        
+        $query = 'SELECT * FROM '.$tableName;
+        $conditions=0;
+        if ($data!="" && is_array($data)){
+            $query .= $this->addWhereStatement($data);
+        }
+        return $query;
     }
     protected function buildPostQuery($data){
-        
+        if ($data!="" && is_array($data)){
+            $query = 'INSERT INTO '.$tableName;
+            $rows = ' (';
+            $values = ' VALUES (';
+            foreach ($data as $row => $value){
+                if ($value === end($data)) {
+                    $rows .= $row.')';
+                    $values .= $value.')';
+                } else {
+                    $rows .= $row.', ';
+                    $values .= $value.', ';
+                }
+            }
+            $query .= $rows.$value;
+        }
+        return $query;
     }
     protected function buildPutQuery($data){
-        
+        if ($data!="" && is_array($data)){
+            $query = 'UPDATE '.$tableName.' SET ';
+            foreach ($data[1] as $row => $value){
+                $query .= $row.'='.$value;
+                if ($value === end($data[1])) $query .= ' ';
+                else $query .= ', ';
+            }
+            $query .= $this->addWhereStatement($data[0]);
+        }
+        return $query;
     }
-    protected function buildUpdateQuery($data){
-        
+    protected function buildDeleteQuery($data){
+        //$sql = "DELETE FROM usuario WHERE data='$data'";
+        $query = 'DELETE FROM '.$tableName;
+        $query .= $this->addWhereStatement($data);
+        return $query;
     }
 }
--- backend/models/ModelController.class.php
+++ backend/models/ModelController.class.php
@@ -2,65 +2,31 @@
 class ModelController extends ControllerCore{
 
     protected function GET($data){
-        $query=buildGetQuery($data);
-        //$sql = "SELECT * FROM usuario WHERE data='$data'";
-        return runQuery($query)->fetch_object();
+        $query=$this->buildGetQuery($data);
+        return $this->runQuery($query)->fetch_object();
     }
 
     protected function POST($data){
-        /*
-        $data=$data[usuario];
-        $passwd=$data[pass];
-        $name=$data[nombre];
-        $dni=$data[DNI];
-        $sex=$data[sexo];
-        $birthdate=$data[fecha_nacimiento];
-        $age=$data[edad];
-        $country=$data[pais];
-        foreach ($data[idioma] as $indice) {
-            $language=$language."$indice:";
+        if ($this->GET($data[0])==""){ //No se exàctament que tornarà una query fallida així que podria no funcionar
+            $query=$this->buildPostQuery($data);
+            return $this->runQuery($query);
+        } else {
+            return null;
         }
-        $comment=$data[observaciones];
-        foreach ($data[aficion] as $indice) {
-            $hobby=$hobby."$indice:";
-        }
-        $sql = " INSERT INTO usuario (data, pass, name, dni, sex, birthdate, age, country, language, comment, hobby)"
-            . " VALUES ('$data', '$passwd', '$name', '$dni', '$sex', '$birthdate', '$age', '$country', '$language', '$comment', '$hobby')";
-        */
-        $query=buildPostQuery($data);
-        return runQuery($query);
     }
     
     protected function PUT($data){
-        /*
-        $data=$data[usuario];
-        $passwd=$data[pass];
-        $name=$data[nombre];
-        $dni=$data[DNI];
-        $sex=$data[sexo];
-        $birthdate=$data[fecha_nacimiento];
-        $age=$data[edad];
-        $country=$data[pais];
-        foreach ($data[idioma] as $indice) {
-            $language=$language."$indice:";
-        }
-        $comment=$data[observaciones];
-        foreach ($data[aficion] as $indice) {
-            $hobby=$hobby."$indice:";
+        if ($this->GET($data[0])!=""){ //No se exàctament que tornarà una query fallida així que podria no funcionar
+            $query=$this->buildPutQuery($data);
+            return $this->runQuery($query);
+        } else {
+            return null;
         }
         
-        $sql = " UPDATE usuario SET pass='$passwd', name='$name', dni='$dni', sex='$sex', birthdate='$birthdate', age='$age',"
-            . " country='$country', language='$language', comment='$comment', hobby='$hobby' WHERE data='$data'";
-        */
-        $query=buildPutQuery($data);
-        return runQuery($query);
     }
     
     protected function DELETE($data){
-        $query=buildDeleteQuery($data);
-
-        //$sql = "DELETE FROM usuario WHERE data='$data'";
-        
-        return runQuery($query);
+        $query=$this->buildDeleteQuery($data);        
+        return $this->runQuery($query);
     }
 }
